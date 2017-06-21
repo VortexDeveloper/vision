9681bcc33aea89d225750d97b9512ebff66f0235
Pedro de Albuquerque <pedro.de.albuquerque@hotmail.com>
2017-06-21 16:20:32 -0300

    change score from products

---
+++ db/migrate/20170621191616_change_score_from_products.rb
@@ -0,0 +1,5 @@
+class ChangeScoreFromProducts < ActiveRecord::Migration[5.0]
+  def change
+    change_column :spree_products, :score, :integer, default: 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170613163128) do
+ActiveRecord::Schema.define(version: 20170621191616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -410,7 +410,7 @@ ActiveRecord::Schema.define(version: 20170613163128) do
     t.boolean  "promotionable",        default: true
     t.string   "meta_title"
     t.datetime "discontinue_on"
-    t.integer  "score"
+    t.integer  "score",                default: 0
     t.index ["available_on"], name: "index_spree_products_on_available_on", using: :btree
     t.index ["deleted_at"], name: "index_spree_products_on_deleted_at", using: :btree
     t.index ["discontinue_on"], name: "index_spree_products_on_discontinue_on", using: :btree
