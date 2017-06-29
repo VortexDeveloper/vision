4bdcdbf81aa4949647a8ac6d2526c9ae3a16eab3
Pedro Antunes Ribeiro César de Albuquerque <pedro.de.albuquerque@hotmail.com>
2017-06-29 12:12:40 -0300

    survey

--- app/controllers/spree/surveys_controller.rb
+++ app/controllers/spree/surveys_controller.rb
@@ -3,4 +3,17 @@ class Spree::SurveysController < Spree::StoreController
     @survey = Spree::Survey.new
     @survey.order_id = params[:order_id]
   end
+
+  def create
+    @survey = Spree::Survey.new(survey_params)
+    @survey.save
+    redirect_to root_path
+    flash.notice = "Sucesso"
+  end
+
+  def survey_params
+    params.require(:survey).permit(
+      :name, :email, :age, :cpf, :phone_number, :order_id
+    )
+  end
 end
